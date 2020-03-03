590d5c38156ec8c32959883d6a7884c9fee26e04
Ha-g <you@example.com>
2020-03-02 17:53:58 -0800

    pic update3

--- src/Brand.jsx
+++ src/Brand.jsx
@@ -18,6 +18,6 @@ const Brand =
 
 <h1 style={{fontStyle: 'bold', fontfamily:'times new roman'}}>About Me</h1> <br/>
 <h4 style={{fontSize: '19px',fontfamily:'times new roman', fontWeight: 'normal', display:'inline'}}>I am a Seattle based front-end web developer who aims to design effective and appealing websites for user interaction. My experience and knowledge in programming languages such as HTML, CSS and JavaScript allow me to pursue my passion to make great websites. My transition in customer service roles fueled this passion for user-centered development. </h4>
-<img style={{borderRadius: '50%', marginTop: '20px', display:'inline'}}src="/img/Me.JPG" height='180px'width="180px"alt=""/>
+<img style={{borderRadius: '50%', marginTop: '20px', display:'inline'}}src="https://avatars3.githubusercontent.com/u/52905657?s=400&v=4" height='180px'width="180px"alt=""/>
 </>
 export default Brand;
